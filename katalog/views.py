e9b65b4ad7c5b2a222022d9e08f5c12fde2e8d7e
farkhans <farkhansyawalharahap@gmail.com>
2022-09-12 14:50:18 +0700

    Submit Tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def katalog(request):
+    data_katalog_item = CatalogItem.objects.all()
+    context = {
+        'list_item' : data_katalog_item,
+        'nama' : 'Farkhan Syawal Harahap',
+        'id' : 2106709125
+    }
+    return render(request, "katalog.html", context)
--- project_django/urls.py
+++ project_django/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from katalog.views import katalog
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
+    path('katalog/', include('katalog.urls')),
 ]
 
 # TODO: Implement Routings Here
