31087c1c33359070d261d400259780bb89276e07
farkhans <farkhansyawalharahap@gmail.com>
2022-09-21 22:06:13 +0700

    Submit Tugas 3

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -4,6 +4,7 @@ from mywatchlist.views import watchlist_html, watchlist_json, watchlist_xml
 app_name = 'mywatchlist'
 
 urlpatterns = [
+    path('', watchlist_html, name = "watchlist_html"),
     path('html/', watchlist_html, name = "watchlist_html"),
     path("xml/", watchlist_xml, name = "watchlist_xml"),
     path("json/", watchlist_json, name = "watchlist_json"),
